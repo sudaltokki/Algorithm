30791ea7171ac6b48ebe46be8d0f6ea23471932e
박지원 <mary000605@naver.com>
2022-10-18 07:34:16 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -6,7 +6,7 @@ int main() {
 	cin >> n;
 
 	for (int i = 1; i < n + 1; i++) {
-		for (int j = 0; j < ((2 * n - 1) - (2 * i - 1)) / 2; j++) {
+		for (int j = 0; j < n - i; j++) {
 			cout << " ";
 		}
 		for (int j = 0; j < 2 * i - 1; j++) {
