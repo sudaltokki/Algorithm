8706dc1281ed44f2cf20835b5ff458b5468a220f
박지원 <mary000605@naver.com>
2022-10-25 15:09:53 +0900

    [Silver III] Title: 두 수의 합, Time: 8 ms, Memory: 6316 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, a[100000], x, tmp[2000001], num = 0;
+int n, a[100000], x, tmp[1000001], num = 0;
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		cin >> a[i];
@@ -12,9 +13,9 @@ int main(void) {
 	cin >> x;
 
 	for (int i = 0; i < n; i++) {
-		if (x - a[i] > 0 && tmp[x - a[i]] == 1) num++;
-        tmp[a[i]]++;
-	}
+		if (x - a[i] > 0 && x - a[i] <= 1000000 &&tmp[x - a[i]] > 0) num++;
+	    tmp[a[i]]++;
+    }
 
 	cout << num;
 }
