a21d20a99a0f3a5ba90335a6850ac4fc5afa48c6
박지원 <mary000605@naver.com>
2022-11-27 20:08:11 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,57 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 #define X first
 #define Y second
+
 int board[51][51];
 bool vis[51][51];
-int t, m, n, k;
-int arr[2500][2];
-int dx[4] = { 1, 0, -1, 0 };
-int dy[4] = { 0 , 1, 0, -1 };
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+int m, n, k;
+queue <pair<int, int >> q;
+
+void bfs(int x, int y) {
+  vis[x][y] = true;
+  q.push({ x,y });
+  while (!q.empty()) {
+    auto cur = q.front(); q.pop();
+    for (int dir = 0; dir < 4; dir++) {
+      int nx = cur.X + dx[dir];
+      int ny = cur.Y + dy[dir];
+      if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+      if (vis[nx][ny] || board[nx][ny] != 1) continue;
+      vis[nx][ny] = true;
+      q.push({ nx,ny });
+    }
+  }
+}
 
 int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	queue<pair<int, int>> Q;
-
-	cin >> t;
-	while (t--) {
-		int ans = 0;
-
-		cin >> m >> n >> k;
-		for (int i = 0; i < k; i++) {
-			cin >> arr[i][1];
-			cin >> arr[i][0];
-			board[arr[i][0]][arr[i][1]] = 1;
-		}
-
-		for (int i = 0; i < k; i++) {
-			int tmpx = arr[i][0];
-			int tmpy = arr[i][1];
-			if (board[tmpx][tmpy] == 1 && !vis[tmpx][tmpy]) {
-				Q.push({ tmpx, tmpy });
-				vis[tmpx][tmpy] = 1;
-				while (!Q.empty()) {
-					pair<int, int> cur = Q.front();
-					Q.pop();
-					for (int dir = 0; dir < 4; dir++) {
-						int nx = cur.X + dx[dir];
-						int ny = cur.Y + dy[dir];
-						if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-						if (vis[nx][ny] || board[nx][ny] != 1) continue;
-						vis[nx][ny] = 1;
-						Q.push({ nx,ny });
-					}
-				}
-				ans++;
-			}
-		}
-
-		cout << ans << "\n";
-		for (int i = 0; i < n; i++) {
-			fill(board[i], board[i] + m, 0);
-			fill(vis[i], vis[i] + m, false);
-		} //초기화를 안시킴 ㅜㅜ
-	}
-
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    cin >> m >> n >> k;
+    int x, y;
+    for (int i = 0; i < k; i++) {
+      cin >> x >> y;
+      board[y][x] = 1;
+    }
+    int res = 0; //지렁이 개수
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (board[i][j] == 1 && !vis[i][j]) {
+          bfs(i, j);
+          res++;
+        }
+      }
+    }
+    cout << res << "\n";
+    for(int i = 0; i < n; i++){
+      fill(board[i], board[i]+m, 0);
+      fill(vis[i], vis[i]+m, false);
+    }
+  }
+  return 0;
 }
