1a5be359467ed53f6413215079275e7043ee9748
박지원 <mary000605@naver.com>
2024-01-05 17:05:45 +0900

    Time: 48 ms (69.41%), Space: 18.8 MB (16.27%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,12 +1,7 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # 자료형 데크로 선언
-        strs: Deque = collections.deque()
-            
-        for c in s:
-            if c.isalnum():
-                strs.append(c.lower())
-                
-        while len(strs) > 1:
-            if strs.popleft() != strs.pop(): return False
-        return True
+        s = s.lower()
+        # 정규식으로 불필요한 문자 필터링
+        s = re.sub('[^a-z0-9]', '', s)
+        
+        return s == s[::-1]
