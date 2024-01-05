7866fdfdbf1c7a3f6a0cee569d38ba61f5d83977
박지원 <mary000605@naver.com>
2024-01-05 16:28:33 +0900

    Time: 229 ms (5.19%), Space: 23.4 MB (5.91%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,8 +1,11 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s1 = s.lower()
-        s1 = ''.join(c for c in s1 if c.isalnum())
-        num = len(s1) // 2
-        for a in range(num):
-            if s1[a] != s1[-1-a]: return False
+        strs = []
+        for c in s:
+            if c.isalnum():
+                strs.append(c.lower())
+                
+        # 팰린드롬 여부 판별
+        while len(strs) > 1:
+            if strs.pop() != strs.pop(0): return False
         return True
