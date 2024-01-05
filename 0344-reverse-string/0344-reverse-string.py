25618472c44af1351672382093d2b3a9912e132e
박지원 <mary000605@naver.com>
2024-01-05 19:00:34 +0900

    Time: 183 ms (79.81%), Space: 21.6 MB (20.58%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,4 +3,11 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        s.reverse()
+        str_len = len(s)
+        bias_left = 0
+        bias_right = str_len - 1
+        
+        for i in range(str_len//2):
+            s[bias_left], s[bias_right] = s[bias_right], s[bias_left]
+            bias_left += 1
+            bias_right -= 1
